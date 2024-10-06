c00b8cbf9ec5a3162415e7754b4a6d906d79b88a
J-Oswalt <josephoswalt95@gmail.com>
2024-10-06 17:59:38 -0400

    Second Commit.

---
+++ public/js/script.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    const allButtons = document.querySelectorAll('.searchBtn');
+    const searchBar = document.querySelector('.searchBar');
+    const searchInput = document.querySelectorAll('.searchInput');
+    const searchClose = document.querySelectorAll('.searchClose');
+
+    for (var i = 0; i < allButtons.length; i++) {
+        allButtons[i].addEventListener('click', function() {
+            searchBar.style.visablility = 'visible';
+            searchBar.classList.add('open');
+            this.setAttribute('aria-expanded', 'true');
+            searchInput.focus();
+        });
+    }
+
+        searchClose.addEventListener('click', function() {
+            searchBar.style.visablility = 'hidden';
+            searchBar.classList.remove('open');
+            this.setAttribute('aria-expanded', 'false');
+        });
+
+
+});
